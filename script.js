8cd85af6467993bfaf8c179989ec6adefd6a8a9c
Natezone24 <zahcghrant@gmail.com>
2022-05-25 19:30:32 -0700

    RESET #7532864876873432

---
+++ script.js
@@ -0,0 +1,27 @@
+function free(){
+  document.write(alert('Installing Free Bitcoin real not virus money $$$$$$$$') + alert(Math.random() + ' Bitcoin.') + alert("Sending to your account...") + alert('Verifying...') + alert('Auto Human verification failed.') + alert('Please go to this site.') + alert('https://bit.ly/3lC23dR'))
+}
+function gc(){
+  window.location.href="GeicoInsurance.html";
+}
+function nfts(){
+  window.location.href="nfts.html";
+}
+function back(){
+  window.location.href="index.html";
+}
+function life(){
+  window.location.href="lifeinsurance.html"
+}
+function politics(){
+  window.location.href='politics.html'
+}
+function fungames(){
+  window.location.href='fungames.html'
+}
+function economy(){
+  window.location.href='economy.html'
+}
+function Health(){
+  window.location.href='Elderlyhealth.html'
+}
